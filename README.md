67876676bb104a021a767564ec0405b6cf2a496a
Hozefa <hozefasailanawala786@gmail.com>
2024-06-17 16:10:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pedometer_app
+ This is a Pedometer app in Flutter that allows for continuous step counting and pedestrian status using the built-in pedometer sensor API of iOS and Android devices.
---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:pedometer/pedometer.dart';
+import 'dart:async';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pedometer App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Stream<StepCount> _stepCountStream;
+  late Stream<PedestrianStatus> _pedestrianStatusStream;
+  String _status = 'stopped', _steps = '';
+  List _mySteps = [];
+
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Pedometer App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Steps Taken',
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              _mySteps.isNotEmpty ? _mySteps.last.toString() : "0",
+              style: TextStyle(fontSize: 50),
+            ),
+            Divider(
+              height: 70,
+              thickness: 0,
+              color: Colors.white,
+            ),
+            Text(
+              'Pedestrian Status',
+              style: TextStyle(fontSize: 30),
+            ),
+            Icon(
+              _status == 'walking'
+                  ? Icons.directions_walk
+                  : _status == 'stopped'
+                      ? Icons.accessibility_new
+                      : Icons.error,
+              size: 100,
+            ),
+            Text(
+              _status,
+              style: _status == 'walking' && _status == 'stopped'
+                  ? TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
+                  : TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void initPlatformState() {
+    _pedestrianStatusStream = Pedometer.pedestrianStatusStream;
+    _pedestrianStatusStream
+        .listen(onPedestrianStatusChanged)
+        .onError(onPedestrianStatusError);
+
+    _stepCountStream = Pedometer.stepCountStream;
+    _stepCountStream.listen(onStepCount).onError(onStepCountError);
+
+    if (!mounted) return;
+  }
+
+  void onStepCount(StepCount event) {
+    setState(() {
+      _mySteps.add(event.steps);
+      //_steps = event.steps.toString();
+    });
+  }
+
+  void onPedestrianStatusChanged(PedestrianStatus event) {
+    setState(() {
+      _status = event.status;
+    });
+  }
+
+  void onPedestrianStatusError(error) {
+    setState(() {
+      _status = 'Pedestrian Status not available';
+    });
+  }
+
+  void onStepCountError(error) {
+    setState(() {
+      _steps = 'Step count not available';
+    });
+  }
+}
